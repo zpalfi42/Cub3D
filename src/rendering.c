bf194b0b6231d0bc4116e49c2467273cfbcb53fe
Zsolt Palfi <zpalfi@car11s1.42barcelona.com>
2022-11-02 18:30:45 +0100

    Finish day 2/11/2022. Started printing, still some problems.

--- src/rendering.c
+++ src/rendering.c
@@ -6,7 +6,7 @@
 /*   By: zpalfi <zpalfi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/02 16:48:08 by zpalfi            #+#    #+#             */
-/*   Updated: 2022/11/02 17:37:28 by zpalfi           ###   ########.fr       */
+/*   Updated: 2022/11/02 18:29:05 by zpalfi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,7 +56,7 @@ void	rendering(t_data *data, int x, int y)
 			WIDTH, HEIGHT, "Cub3D");
 	if (!data->win_ptr)
 		return ;
-	while (++x < 10)
+	while (++x < WIDTH)
 	{
 		printf("%f %f %f %f - ", data->dirx, data->diry, data->planex, data->planey);
 		data->hit = 0;
@@ -65,11 +65,9 @@ void	rendering(t_data *data, int x, int y)
 		data->raydiry = data->diry + data->planey * data->camerax;
 		data->mapx = (int)data->posx;
 		data->mapy = (int)data->posy;
-		printf("%f %f %f\n", data->camerax, data->raydirx, data->raydiry);
-		data->deltadistx = sqrt(1 + (data->raydiry * data->raydiry)
-				/ (data->raydirx * data->raydirx));
-		data->deltadisty = sqrt(1 + (data->raydirx * data->raydirx)
-				/ (data->raydiry * data->raydiry));
+		// printf("%f %f %f\n", data->camerax * data->planex, data->raydirx, data->raydiry);
+		data->deltadistx = fabs(1 / data->raydirx);
+		data->deltadisty = fabs(1 / data->raydiry);
 		if (data->dirx < 0)
 		{
 			data->stepx = -1;
@@ -88,12 +86,13 @@ void	rendering(t_data *data, int x, int y)
 		}
 		else
 		{
-			data->stepy = 0;
+			data->stepy = 1;
 			data->sidedisty = (data->mapy + 1.0 - data->posy) * data->deltadisty;
 		}
-		printf("Hola %f %f %f %f\n", data->sidedistx, data->deltadisty, data->deltadistx, data->deltadisty);
+		// printf("Hola %f %f %f %f\n", data->sidedistx, data->sidedisty, data->deltadistx, data->deltadisty);
 		while (data->hit == 0)
 		{
+			// printf("%d %d\n", data->mapx, data->mapy);
 			if (data->sidedistx < data->sidedisty)
 			{
 				data->sidedistx += data->deltadistx;
@@ -112,25 +111,34 @@ void	rendering(t_data *data, int x, int y)
 				data->hit = 1;
 			}
 		}
-		printf("Adios\n");
 		if (data->side == 0)
 		{
-			data->perpwalldist = (data->mapx - data->posx
-					+ (1 + data->stepx) / 2) / data->raydirx;
+			data->perpwalldist = (data->mapx - data->posx + (1 - data->stepx) / 2) / data->raydirx;
 		}
 		else
 		{
-			data->perpwalldist = (data->mapy - data->posy
-					+ (1 + data->stepy) / 2) / data->raydiry;
+			data->perpwalldist = (data->mapy - data->posy + (1 - data->stepy) / 2) / data->raydiry;
 		}
 		data->line = (int)(HEIGHT / data->perpwalldist);
-		data->drawstart = -data->line / 2 + HEIGHT / 2;
-		if (data->drawstart < 0)
-			data->drawstart = 0;
-		data->drawend = data->line / 2 + HEIGHT / 2;
-		if (data->drawend >= HEIGHT)
-			data->drawend = 0;
-		// printf("--> %d %d\n", data->drawstart, data->drawend);
+		data->drawstart = (-data->line / 2) + (HEIGHT / 2);
+		// if (data->drawstart < 0)
+		// 	data->drawstart = 0;
+		data->drawend = (data->line / 2) + (HEIGHT / 2);
+		// if (data->drawend >= HEIGHT)
+		// 	data->drawend = 0;
+		y = data->drawstart - 1;
+		while (++y < data->drawend)
+		{
+			if (data->side == 0)
+				mlx_pixel_put(data->mlx_ptr, data->win_ptr, x, y, (get_color(data->f, 0, -1, 0) << 16
+						| get_color(data->f, 1, -1, 0) << 8
+						| get_color(data->f, 2, -1, 0)));
+			else
+				mlx_pixel_put(data->mlx_ptr, data->win_ptr, x, y,(get_color(data->c, 0, -1, 0) << 16
+						| get_color(data->c, 1, -1, 0) << 8
+						| get_color(data->c, 2, -1, 0)));
+		}
+		printf("--> %d %d\n", data->drawstart, data->drawend);
 	}
 	mlx_hook(data->win_ptr, 2, 0, key_handler, &data);
 	mlx_loop(data->mlx_ptr);
