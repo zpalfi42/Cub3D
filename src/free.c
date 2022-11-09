826be1d6e48b7aad173a732c059f8703a07c9a0b
Zsolt Palfi <zpalfi@cbr4s4.42barcelona.com>
2022-11-09 15:55:47 +0100

    Final version, no leaks, all perfect

--- src/free.c
+++ src/free.c
@@ -6,7 +6,7 @@
 /*   By: zpalfi <zpalfi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/26 11:32:59 by zpalfi            #+#    #+#             */
-/*   Updated: 2022/11/08 16:58:30 by zpalfi           ###   ########.fr       */
+/*   Updated: 2022/11/09 15:54:45 by zpalfi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,7 +31,6 @@ void	free_map(t_data *data, int i)
 	mlx_destroy_image(data->mlx_ptr, data->textures[3].img);
 	mlx_destroy_image(data->mlx_ptr, data->img);
 	mlx_destroy_window(data->mlx_ptr, data->win_ptr);
-	free(data->mlx_ptr);
 	while (data->map[i] != NULL && i < data->height + 2)
 	{
 		free(data->map[i]);
